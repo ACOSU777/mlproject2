4fcc682deed43f5a6fd8500e43da27171640369d
Liang-Hung Chen <blackparade0518@gmail.com>
2025-01-01 16:42:21 -0500

    Data Transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass # check the function of decorator @ & dataclass, and class function, if only define the variable, we could just use dataclass. If multifunction,it's better to have constructure here
 class DataIngestionConfig:
@@ -46,4 +48,7 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
