cb74a98164c1e36dd6dceee8d084b0084cf0240a
Liang-Hung Chen <blackparade0518@gmail.com>
2025-01-04 17:38:14 -0500

    Hyperparameter Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
